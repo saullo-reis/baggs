12cb5236f4b37f46e1370e2b942476fb194b6a8a
Saullo Reis <saulloreis01@hotmail.com>
2023-07-26 12:02:54 -0300

    fix: Arrumando bug de duplicação de nomes/email

--- api/src/Database/Database.ts
+++ api/src/Database/Database.ts
@@ -9,10 +9,24 @@ const database = mysql.createConnection({
 
 database.connect();
 
-database.query('create table IF NOT EXISTS enterprises (id INTEGER AUTO_INCREMENT PRIMARY KEY, NAME_ENTERPRISE TEXT NOT NULL, PASSWORD TEXT, CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP, EMAIL TEXT NOT NULL ); ', (err) => {
-    if(err) return console.error(err.message)
-})
-database.query('create table IF NOT EXISTS inventory (id_item INTEGER AUTO_INCREMENT PRIMARY KEY, ID_ENTERPRISE INTEGER NOT NULL UNIQUE, NAME_ITEM VARCHAR(255) NOT NULL, QNT_ITEM INTEGER, QNT_MIN_ITEM INTEGER);', (err) => {
+database.query('CREATE TABLE IF NOT EXISTS enterprises (\
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,\
+    NAME_ENTERPRISE VARCHAR(255) NOT NULL UNIQUE,\
+    PASSWORD TEXT,\
+    CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
+    EMAIL VARCHAR(255) NOT NULL UNIQUE\
+);', (err) => {
+    if(err) return console.error(err.message);
+    
+});
+
+database.query('create table IF NOT EXISTS inventory (\
+    id_item INTEGER AUTO_INCREMENT PRIMARY KEY,\
+    ID_ENTERPRISE INTEGER NOT NULL UNIQUE,\
+    NAME_ITEM VARCHAR(255) NOT NULL,\
+    QNT_ITEM INTEGER,\
+    QNT_MIN_ITEM INTEGER\
+    );', (err) => {
     if (err) return console.error(err.message)
 })
 
