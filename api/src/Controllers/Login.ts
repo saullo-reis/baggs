d327af2f3abe1f27b7ace7bc9013cd1c2e8f0aee
Saullo Reis <saulloreis01@hotmail.com>
2023-07-27 12:05:50 -0300

    feat: adicionando o controller login

---
+++ api/src/Controllers/Login.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import sha256 from "sha256";
+import { database } from "../Database/Database";
+import { StatusCodes } from 'http-status-codes'
+
+function checkEmailExist(email: string): Promise<boolean> {
+    const query = 'SELECT EMAIL FROM companies WHERE EMAIL = ?'
+
+    return new Promise((resolve, reject) => {
+        database.query(query, email, (err, results) => {
+            if (err) {
+                console.error(err)
+                reject(err)
+            }
+
+            resolve(results.length === 1)
+        })
+    })
+}
+
+function checkPasswordIsCorrect(email: string, password: string): Promise<boolean> {
+    const query = 'SELECT PASSWORD FROM companies WHERE email = ?'
+
+    return new Promise((resolve, reject) => {
+        database.query(query, email, (err, results) => {
+            if (err) {
+                console.error(err)
+                reject(err)
+            }
+
+            resolve(password === results[0].password)
+        })
+    })
+
+}
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body
+    const emailExist = await checkEmailExist(email)
+    const passwordIsCorrect = await checkPasswordIsCorrect(email, sha256(password))
+
+    if (!emailExist) return res.status(StatusCodes.BAD_REQUEST).send({
+        message: {
+            error: 'Email não registrado.'
+        }
+    })
+
+    if (!passwordIsCorrect) return res.status(StatusCodes.BAD_REQUEST).send({
+        message: {
+            error: 'Senha incorreta.'
+        }
+    })
+    
+    return res.status(StatusCodes.ACCEPTED).send({
+        sucess: '92hj4891hj9r1h237u98r129hr7912h98211'
+    })
+}
