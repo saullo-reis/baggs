47815198ba59ddf82254a6bbc418cf8deabc3d96
Saullo Reis <saulloreis01@hotmail.com>
2023-07-27 12:05:08 -0300

    refactor: modificando nome de enterprises para companies e adicionando status

--- api/src/Controllers/Register.ts
+++ api/src/Controllers/Register.ts
@@ -1,18 +1,19 @@
 import {Request, Response} from "express"
 import { database } from "../Database/Database"
 import sha256 from 'sha256'
+import { StatusCodes } from "http-status-codes";
 
 export const register = async (req: Request, res: Response) => {
-    const query = 'INSERT INTO enterprises (NAME_ENTERPRISE, PASSWORD, EMAIL) VALUES (?, ?, ?)'
+    const query = 'INSERT INTO companies (NAME_COMPANY, PASSWORD, EMAIL) VALUES (?, ?, ?)'
     const { name, password, email } = req.body;
 
     try{
         database.query(query, [name, sha256(password), email], (err) => {
             if (err){
                 console.log(err)
-                return res.send(err.message);
+                return res.status(StatusCodes.BAD_GATEWAY).send(err.message);
             } 
-            return res.send('Registrado');
+            return res.status(StatusCodes.ACCEPTED).send('Registrado');
         });
     }catch(err){
         console.error(err)
